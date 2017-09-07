ca18a029aaf85f35c0abbac30a4909a237655ecf
Andrew Presnell <andrew_presnell@hotmail.com>
2017-09-07 16:46:00 -0700

    added table to mixing bowl. reconfigured adding function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,6 +50,10 @@ $(document).ready(function() {
       	quantity,
       	unit:unit
       });
+
+      //clear the form
+      $("#form-ingredient").val("");
+      $("#form-quantity").val("");
     });
 
     //Add items to pantry
@@ -79,19 +83,27 @@ $(document).ready(function() {
     	itemToAdd += ",";
     })
 
-    $("#addToBowl").on("click", function() {
-
-    })
-
     //Add items to Mixing Bowl
     $("#addToBowl").on("click", function() {
       console.log($("#pantry-list").children("tr").length);
       for (var i = 0; i < $("#pantry-list").children("tr").length; i++) {
         //if this item is checked add to mixing bowl list
+        
+
         if ($("#pantry-list").children("tr").eq(i).children("td").eq(3).children("input").is(':checked')) {
-          $(".ingredientList").append("<li><span ingredient-name=\"" + $("#pantry-list").children("tr").eq(i).children("td").eq(0).text() + "\">" + $("#pantry-list").children("tr").eq(i).children("td").eq(0).text() + "</span></li>");
+          $("#mixingBowlList").append("<tr><td><span ingredient-name=\"" + $("#pantry-list").children("tr").eq(i).children("td").eq(0).text() + "\">"
+           + $("#pantry-list").children("tr").eq(i).children("td").eq(0).text() + "</span></td><td><span>"
+           + $("#pantry-list").children("tr").eq(i).children("td").eq(1).text() + "</span></td><td><span>"
+           + $("#pantry-list").children("tr").eq(i).children("td").eq(2).text() + "</span></td><td>"
+           + "<a class=\"btn-floating btn red box\"><i class=\"material-icons deleteBowlItemButton\">delete_forever</i></a>"
+           );
         }
       }
+    });
+
+    // Delete Items from Mixing Bowl
+    $(document).on("click", ".deleteBowlItemButton", function() {
+      $(this).closest("tr").remove();
     });
     
     //Delete Pantry Item
@@ -104,11 +116,11 @@ $(document).ready(function() {
 
 //  ***Spoonacular API***
 	//initialize variables
-    var diet;
-  	var include;
-  	var exclude;
-  	var calories;
-  	var type;
+    var diet = "";
+  	var include = "";
+  	var exclude = "";
+  	var calories = "";
+  	var type = "";
   
   	// Initialize Firebase
   	var config = {
@@ -119,92 +131,93 @@ $(document).ready(function() {
   	  storageBucket: "spoonaculartest.appspot.com",
   	  messagingSenderId: "362221940130"
   	};
-  	  $("#select").on("click", function(){
-        event.preventDefault();
-        var ingredientList = $(".ingredientList").children("li");
 
-        include = "";
-        for (var i = 0; i < ingredientList.length; i++) {
-          //add ingredients to api parameter
-          include += ingredientList.eq(i).text() + ",";
-        }
-        //remove comma from last ingredient
-        include = include.slice(0, -1);
-        console.log(include);
+    // Search For Recipes
+	  $("#select").on("click", function(){
+      event.preventDefault();
+      var ingredientList = $("#mixingBowlList").children("tr");
 
-        //search through diets and find selected
-        diet = "&diet=";
-        diet +=  $(".dietDropdown ul li.selected").text().replace(/\s/g, "");
+      include = "";
+      for (var i = 0; i < ingredientList.length; i++) {
+        //add ingredients to api parameter
+        include += ingredientList.eq(i).children("td").eq(0).text() + ",";
+      }
+      //remove comma from last ingredient
+      include = include.slice(0, -1);
+      console.log(include);
+
+      //search through diets and find selected
+      diet = "&diet=";
+      diet = "&diet=" + $(".dietDropdown ul li.selected").text().replace(/\s/g, "");
+      
+  		/*exclude = "&excludeIngredients=" + $(".exclude").val().trim();*/
+  	  calories = "&maxCalories=" + $(".calories").val().trim();
+
+      //search through type and find selected
+      type = "&type=";
+      type = "&type=" + $(".courseDropdown ul li.selected").text().replace(/\s/g, "");
+
+  		var queryURL = 
+  	  "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?includeIngredients=" + 
+  	  include + 
+  	  diet +
+  	  exclude + 
+  	  calories + 
+  	  type + 
+  	  "&ranking=2&number=6&instructionsRequired=true&addRecipeInformation=true&mashape-key=EtOafYwxEJmsh9OKoxDdDksedhQLp1gkmXbjsnR7Wi1CzQDwpd";
+
+      console.log()
+		$.ajax({
+		  url: queryURL,
+		  method: "GET"
+	  }).done(function(response) {
+		  console.log(response);
+      //Get length of results to display that number of cards
+      var responseLength = response.results.length;
+
+      //clear old cards
+      $("#recipe-cards").empty();
+      //start creating the cards
+      for (var i = 0; i < responseLength; i++) {
+        var cardDiv = $("<div class=\"col s12 m4 cardDiv\">");
+        var card = $("<div class=\"card\">").addClass("medium");
+        var cardImage = $("<div class=\"card-image\">");
+        var image = $("<img>");
+        var cardContent = $("<div class=\"card-content\">");
+        var cardTitle = $("<span class=\"card-title\">");
+        var cardParagraph = $("<p class=\"cardParagraph\">");
+        var cardTime = $("<span class=\"cardTime\">");
+        var cardScore = $("<span class=\"cardScore\">");
+        /*var cardDescription = $("<span class=\"cardDescription\">");*/
+        var cardAction = $("<div class=\"card-action\">");
+        var cardSource = $("<a class=\"cardsource\">");
+
+        //add img src
+        image.attr("src", response.results[i].image);
+
+        //add card title
+        cardTitle.text(response.results[i].title);
+
+        //add card time
+        cardTime.text("Ready In " + response.results[i].readyInMinutes + " Minutes");
+
+        //add card score
+        cardScore.text("Spoonacular Score: " + response.results[i].spoonacularScore);
+
+        //add card source
+        cardSource.attr("href", response.results[i].spoonacularSourceUrl).text("OPEN SOURCE PAGE");
+
+        $("#recipe-cards").append(cardDiv);
+        cardDiv.append(card);
+        card.append(cardImage).append(cardContent).append(cardAction);
+        cardImage.append(image);
+        cardContent.append(cardTitle).append(cardParagraph);
+        cardParagraph.append(cardTime).append("<br>").append(cardScore)/*.append(cardDescription)*/.append("<br>");
+        cardAction.append(cardSource);
         
-    		exclude = "&excludeIngredients=" + $(".exclude").val().trim();
-    	  calories = "&maxCalories=" + $(".calories").val().trim();
-
-        //search through type and find selected
-        type = "&type=";
-        type += "&type=" + $(".courseDropdown ul li.selected").text().replace(/\s/g, "");
-    		/*type += "&type=" + $(".courseDropdown ul li.selected").text();*/
-
-    		var queryURL = 
-    	  "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?includeIngredients=" + 
-    	  include + 
-    	  diet +
-    	  exclude + 
-    	  calories + 
-    	  type + 
-    	  "&ranking=2&number=6&instructionsRequired=true&addRecipeInformation=true&mashape-key=EtOafYwxEJmsh9OKoxDdDksedhQLp1gkmXbjsnR7Wi1CzQDwpd";
-
-        console.log()
-  		$.ajax({
-  		  url: queryURL,
-  		  method: "GET"
-  	  }).done(function(response) {
-  		  console.log(response);
-        //Get length of results to display that number of cards
-        var responseLength = response.results.length;
-
-        //clear old cards
-        $("#recipe-cards").empty();
-        //start creating the cards
-        for (var i = 0; i < responseLength; i++) {
-          var cardDiv = $("<div class=\"col s12 m4 cardDiv\">");
-          var card = $("<div class=\"card\">").addClass("medium");
-          var cardImage = $("<div class=\"card-image\">");
-          var image = $("<img>");
-          var cardContent = $("<div class=\"card-content\">");
-          var cardTitle = $("<span class=\"card-title\">");
-          var cardParagraph = $("<p class=\"cardParagraph\">");
-          var cardTime = $("<span class=\"cardTime\">");
-          var cardScore = $("<span class=\"cardScore\">");
-          /*var cardDescription = $("<span class=\"cardDescription\">");*/
-          var cardAction = $("<div class=\"card-action\">");
-          var cardSource = $("<a class=\"cardsource\">");
-
-          //add img src
-          image.attr("src", response.results[i].image);
-
-          //add card title
-          cardTitle.text(response.results[i].title);
-
-          //add card time
-          cardTime.text("Ready In " + response.results[i].readyInMinutes + " Minutes");
-
-          //add card score
-          cardScore.text("Spoonacular Score: " + response.results[i].spoonacularScore);
-
-          //add card source
-          cardSource.attr("href", response.results[i].spoonacularSourceUrl).text("OPEN SOURCE PAGE");
-
-          $("#recipe-cards").append(cardDiv);
-          cardDiv.append(card);
-          card.append(cardImage).append(cardContent).append(cardAction);
-          cardImage.append(image);
-          cardContent.append(cardTitle).append(cardParagraph);
-          cardParagraph.append(cardTime).append("<br>").append(cardScore)/*.append(cardDescription)*/.append("<br>");
-          cardAction.append(cardSource);
-          
         }
-  	  });
-  	});
+      });
+    });
 
 
   	//Get a random joke
